aa6a3b4fe66ce595d3ac5af6d5488ebe43100a87
hnchengjianjun <hn_chengjianjun@hotmail.com>
2017-04-10 14:09:21 +0800

    create

---
+++ src/main/java/com/junshao/controller/LoginController.java
@@ -0,0 +1,7 @@
+package com.junshao.controller;
+
+/**
+ * Created by GOODBOY on 2017/4/10.
+ */
+public class LoginController {
+}
---
+++ src/main/test/com/junshao/test/LoginTest.java
@@ -0,0 +1,7 @@
+package com.junshao.test;
+
+/**
+ * Created by GOODBOY on 2017/4/10.
+ */
+public class LoginTest {
+}
